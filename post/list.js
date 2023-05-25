f9ca8f603e540cc5e03fbd320ad71540737f1839
junheiLee <jun2ee22@naver.com>
2023-05-26 01:45:06 +0900

    feat: 게시글 목록에 작성된 제목 표시 기능

--- post/list.js
+++ post/list.js
@@ -1,3 +1,22 @@
 function goToWriteForm() {
     window.location.href = "/post/write/";
 }
+
+async function loadPosts() {
+    const response = await fetch(`http://localhost:8080/posts`);
+    const dataList = await response.json();
+    console.log("dataList={}", dataList);   
+
+    const ulElement = document.getElementById("post-list");
+
+    for(var i=0; i<dataList.length; i++) {
+        const dto = JSON.parse(JSON.stringify(dataList[i]));
+        
+        const liElement = document.createElement('li');
+        liElement.textContent = dto.title;
+        ulElement.appendChild(liElement);
+    }
+    
+}
+
+loadPosts();
