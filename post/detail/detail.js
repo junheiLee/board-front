8748283dc8b9f094a7cce2b91bc12cde9eaeb3fa
junheiLee <jun2ee22@naver.com>
2023-05-26 14:51:34 +0900

    feat: 게시글 삭제 기능

--- post/detail/detail.js
+++ post/detail/detail.js
@@ -1,6 +1,28 @@
 const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get("postId");
 
+// 삭제 확인
+function confirmDelete() {
+  var result = confirm("삭제 할거니?");
+
+  if(result) {
+    deletePost();
+  } else {
+
+  }
+}
+
+// 게시글 삭제
+async function deletePost() {
+  const response = await fetch(`http://localhost:8080/posts/${postId}`, {
+    method: "DELETE",
+  })
+
+  window.location.href = `/post/`;
+}
+
+
+// 게시글 제목, 내용 작성일 불러오기
 async function fetchPostById(postId) {
   
     const response = await fetch(`http://localhost:8080/posts/${postId}`)
@@ -19,10 +41,12 @@ async function fetchPostById(postId) {
 
 }
 
+window.addEventListener('DOMContentLoaded', function() {
+  fetchPostById(postId);
+})
+
+// 목록으로 돌아가기 버튼
 function goToList() {
   window.location.href = "/post/";
 }
 
-window.addEventListener('DOMContentLoaded', function() {
-    fetchPostById(postId);
-})
